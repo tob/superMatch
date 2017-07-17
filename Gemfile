c91e297d27318ab85877f4b77f8959e573172277
supermay <wu_jiamei@outlook.com>
2017-07-17 12:31:02 +0200

    Add feature testing gems

--- Gemfile
+++ Gemfile
@@ -40,6 +40,8 @@ group :development, :test do
   gem 'capybara', '~> 2.9', '>= 2.9.1'
   gem 'factory_girl_rails', '4.7.0'
   gem 'faker', '1.6.6'
+  gem 'selenium-webdriver'
+  gem "chromedriver-helper"
 end
 
 group :development do
@@ -53,6 +55,7 @@ end
 
 group :test do
   gem 'shoulda-matchers', '~> 3.1'
+  gem 'database_cleaner'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,9 @@ Shoulda::Matchers.configure do |config|
     with.library :rails
   end
 end
+
+require 'capybara/rspec'
+require 'capybara/rails'
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -38,7 +41,7 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = false
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -59,4 +62,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+end
+Capybara.register_driver :selenium do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
 end
