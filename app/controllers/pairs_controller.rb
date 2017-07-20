a76ac28d633bd5337f34f92cf34355913922961b
supermay <wu_jiamei@outlook.com>
2017-07-20 21:29:52 +0200

    Update Pairs controller actions and logic

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -1,18 +1,19 @@
 class PairsController < ApplicationController
-  def create
-    @days = Day.all
-    @unassigned_days = []
 
-    @days.each do |day|
-      if day.pairs.first.date == nil
-        @unassigned_days << day
-      end
+  def create
+    get_unassigned_days
+# guarantee that unassigned_days is not empty
+# you generate another 7 days and put in the unassigned_days
+
+    if @unassigned_days == []
+      generate_7_days
+      get_unassigned_days
     end
 
     @pair = Pair.new(pair_params)
     @this_day = @unassigned_days.sample
 
-    @this_day.each do |pair|
+    @this_day.pairs.each do |pair|
       pair.date = @pair.date
     end
 
@@ -24,5 +25,40 @@ class PairsController < ApplicationController
    def pair_params
      params.require(:pair).permit(:date)
    end
+
+
+
+  def get_unassigned_days
+     @days = Day.all
+     @unassigned_days = []
+
+     @days.each do |day|
+       if day.pairs.first.date == nil
+         @unassigned_days << day
+       end
+     end
+   end
+
+
+
+   def generate_7_days
+
+   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 end
