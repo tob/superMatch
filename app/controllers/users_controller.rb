0fc8d90cad7c410616f311fce13e8335f957b385
tob <tobiadonati@gmail.com>
2017-07-21 13:57:59 +0200

    admin view finished

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,10 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+
+  def index
+    @user == current_user
+  end
+
   def show
     @user = User.find(params[:id])
     @profile = current_user.profile
